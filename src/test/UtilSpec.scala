c912c49ac4d9d8ed4bed7135453c5ce28de83120
Yasunori Mahata <mahata777@gmail.com>
2014-05-20 12:59:21 +0900

    Add sha1 test

--- src/test/UtilSpec.scala
+++ src/test/UtilSpec.scala
@@ -16,8 +16,14 @@ class UtilSpec extends Specification {
   }
 
   "Util.base64decode" should {
-    "base64decode \"c2VjcmV0UGEkJHdvcmQ=\" to \"secretPa$$word\"" in {
+    "convert \"c2VjcmV0UGEkJHdvcmQ=\" to \"secretPa$$word\"" in {
       Util.base64decode("c2VjcmV0UGEkJHdvcmQ=") must equalTo("secretPa$$word")
+    }
+  }
+
+  "Util.sha1" should {
+    "covert \"apple\" to \"d0be2dc421be4fcd0172e5afceea3970e2f3d940\"" in {
+      Util.sha1("apple") must equalTo("d0be2dc421be4fcd0172e5afceea3970e2f3d940")
     }
   }
 }
